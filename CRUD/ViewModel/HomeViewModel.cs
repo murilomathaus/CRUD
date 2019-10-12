ea0f62d3335b4fcbd0ee2fb42d4fafc0573a6f8f
Murilo Mathaus <murilomathaus@hotmail.com>
2019-10-12 19:01:13 -0300

    INT: Inicialização de repositório e esqueleto do projeto

---
+++ CRUD/ViewModel/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CRUD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.ViewModel
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public List<Client> Clients { get; set; } = new List<Client>();
+
+        public HomeViewModel()
+        {
+#if DEBUG
+            Clients.Add(new Client() { Name = "José", Age = 32, Index = 1, LastName = "Silva" });
+#endif
+        }
+    }
+}
